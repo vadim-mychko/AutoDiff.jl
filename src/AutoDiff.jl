0ea990bb7be60a81c4119e1b71d66f7d014eece5
Vadim Mychko <vadim_mychko@proton.me>
2024-02-02 00:59:05 +0100

    add Tensor structure template

--- src/AutoDiff.jl
+++ src/AutoDiff.jl
@@ -1,5 +1,12 @@
 module AutoDiff
 
-greet() = print("Hello World!")
+mutable struct Tensor{T <: Real, N <: Integer}
+    data::Array{T, N}
+    grad::Array{T, N}
+    label::String
+    operation::String
+    parents::Set{Tensor}
+    require_grad::Bool
+end
 
 end # module AutoDiff
