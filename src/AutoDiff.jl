19903d174b88a6e3810a06e489c07ae3d4ef5a60
Vadim Mychko <vadim_mychko@proton.me>
2024-02-02 08:09:29 +0100

    overload Base.+ for Tensor struct

--- src/AutoDiff.jl
+++ src/AutoDiff.jl
@@ -1,6 +1,6 @@
 module AutoDiff
 
-import Base
+import Base: show, +
 
 export Tensor
 
@@ -19,19 +19,49 @@ function Tensor(
     label::AbstractString = "",
     operation::AbstractString = "",
     parents::AbstractSet{Tensor} = Set{Tensor}(),
-    require_grad::Bool = false
+    require_grad::Bool = false,
 )
     return Tensor(data, label, operation, parents, require_grad,
                   zeros(Float32, size(data)), () -> return)
 end
 
-function Base.show(io::IO, obj::Tensor)
+function Tensor(data::Real; kwargs...)
+    return Tensor([data]; kwargs...)
+end
+
+function show(io::IO, obj::Tensor)
     print(io, "Tensor(")
     show(io, obj.data)
     !isempty(obj.label) && print(io, ", label=\"$(obj.label)\"")
     !isempty(obj.operation) && print(io, ", operation=\"$(obj.operation)\"")
     obj.require_grad && print(io, ", require_grad=true")
     print(io, ")")
+end
+
+function reshape_grad(grad::AbstractArray, target_shape::Tuple)
+    grad_shape = size(grad)
+    grad_shape == target_shape && return grad
+    target_shape == () && return sum(grad)
+
+    while length(target_shape) != length(grad_shape)
+        push!(target_shape, 1)
+    end
+
+    dims = [i for i in 1:length(target_shape)
+            if target_shape[i] == 1 && grad_shape[i] != 1]
+
+    return reshape(sum(grad; dims), target_shape)
+end
+
+function +(a::Tensor, b::Tensor)::Tensor
+    require_grad = a.require_grad || b.require_grad
+    out = Tensor(a.data .+ b.data; require_grad)
+    out.update! = () -> begin
+        a.grad += reshape_grad(out.grad, size(a.grad))
+        b.grad += reshape_grad(out.grad, size(b.grad))
+    end
+
+    return out
 end
 
 end # module AutoDiff
