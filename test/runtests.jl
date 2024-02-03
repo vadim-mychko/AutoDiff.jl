0bed0ded3e9bff245310cef7903579602d3d0fa7
Vadim Mychko <vadim_mychko@proton.me>
2024-02-03 08:06:52 +0100

    change forward pass tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,46 @@
 using AutoDiff
 using Test
 
-@testset "Forward pass" begin
-    include("test_forward.jl")
-end
+const N_ELEMENTS = 1:10
+const DIM_RANGE = 1:3
 
-@testset "Backward pass" begin
-    include("test_backward.jl")
+@testset "Forward pass" begin
+    @testset "Binary `$op` on random tensors" for op in [+, -, *, /]
+        for n_dims in DIM_RANGE
+            dims = Tuple(rand(N_ELEMENTS) for _ in 1:n_dims)
+            a = rand(Float32, dims)
+            b = rand(Float32, dims)
+            ta = Tensor(a)
+            tb = Tensor(b)
+            @test op(ta, tb).data ≈ op.(a, b)
+        end
+    end
+
+    @testset "Binary `matmul` on random tensors" begin
+        dims = Tuple(rand(N_ELEMENTS) for _ in 1:3)
+        a = rand(Float32, dims[1:2])
+        b = rand(Float32, dims[2:3])
+        ta = Tensor(a)
+        tb = Tensor(b)
+        @test matmul(ta, tb).data ≈ a * b
+    end
+
+    @testset "Unary `$op` on random tensors" for op in [-, inv]
+        for n_dims in DIM_RANGE
+            dims = Tuple(rand(N_ELEMENTS) for _ in 1:n_dims)
+            a = rand(Float32, dims)
+            ta = Tensor(a)
+            @test op(ta).data ≈ op.(a)
+        end
+    end
+
+    @testset "Unary `^` on random tensors" begin
+        for n_dims in DIM_RANGE
+            dims = Tuple(rand(N_ELEMENTS) for _ in 1:n_dims)
+            a = rand(Float32, dims)
+            b = rand()
+            ta = Tensor(a)
+            @test (ta ^ b).data ≈ (a .^ b)
+        end
+    end
 end
