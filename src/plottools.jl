bcacb1025bf6a2a3a916fc709877f4a111729196
Vadim Mychko <vadim_mychko@proton.me>
2024-02-04 06:07:10 +0100

    plot operations on tensors as separate nodes

--- src/plottools.jl
+++ src/plottools.jl
@@ -1,4 +1,4 @@
-import Graphs: DiGraph, add_edge!
+import Graphs: DiGraph, add_edge!, add_vertex!
 import GraphRecipes: graphplot
 import Plots: plot
 
@@ -13,29 +13,41 @@ This recipe can be shown using graphplot(a::Tensor) from Plots library.
 # Arguments
 - `root::Tensor`: The root tensor from which to start plotting the computational graph.
 """
-function show_graph(root::Tensor; nodeshape=:circle, curves=false, node_size=0.3,
+function show_graph(root::Tensor; nodeshape=:circle, curves=false, node_size=0.2,
     fontsize=10, nodecolor=:lightgray, method=:stress, kwargs...)
 
     nodes = topological_sort(root)
-    tensor2id = Dict{Tensor,Int}()
+    tensor2index = Dict{Tensor,Int}()
     for (index, node) in enumerate(nodes)
-        tensor2id[node] = index
+        tensor2index[node] = index
     end
 
     n_nodes = length(nodes)
     g = DiGraph(n_nodes)
     names = Vector{String}(undef, n_nodes)
-    edgelabel = Dict{Tuple{Int,Int},String}()
 
+    n_operations = 0
+    n_unnamed = 0
     for (index, node) in enumerate(nodes)
-        names[index] = node.label
+        if isempty(node.label)
+            n_unnamed += 1
+            names[index] = "t$n_unnamed"
+        else
+            names[index] = node.label
+        end
+
+        isempty(node.parents) && continue
+
+        n_operations += 1
+        operation_index = n_nodes + n_operations
+        add_vertex!(g)
+        push!(names, node.operation)
+        add_edge!(g, operation_index, index)
+
         for parent in node.parents
-            parent_index = tensor2id[parent]
-            add_edge!(g, parent_index, index)
-            edgelabel[(parent_index, index)] = node.operation
+            add_edge!(g, tensor2index[parent], operation_index)
         end
     end
 
-    graphplot(g; names, edgelabel, nodeshape, curves, node_size, fontsize,
-        nodecolor, method, kwargs...)
+    graphplot(g; names, nodeshape, curves, node_size, fontsize, nodecolor, method, kwargs...)
 end
